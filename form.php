73f9f25af49931de4a576cde6ded6e6adde5fc84
baniyanoor-svg <baniyanoor@gmail.com>
2025-12-03 12:28:57 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,12 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+echo "Username: " . $username . "<br>";
+echo "Password: " . $password . "<br>";
+echo "Email: " . $email . "<br>";
+?>
+
+
+
